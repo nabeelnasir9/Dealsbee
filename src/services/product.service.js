e8c4728fe041ce86a58ef243567055fad61fb447
imrancs058 <imrancs058@yahoo.com>
2024-01-16 17:16:07 +0500

    Handling multiple categories

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -3,151 +3,165 @@ import mongoose from "mongoose";
 
 export const ProductService = {
   getProducts: async ({ page = 1, limit = 10, ...query }) => {
-    const skip = (page - 1) * limit;
-    let pipeline = [];
-    if (!query.category_id) {
-      query.category_id = "mobile";
-    } else {
-      query.category_id = query.category_id.replaceAll("_", " ");
-    }
-    const regex = new RegExp(query.category_id, "i");
-    const categories =
-      query.category == "all"
-        ? await CategoryModel.find()
-        : await CategoryModel.find({ name: regex });
-    let categoryIds = [];
-    if (query) {
-      if (categories.length) {
-        categoryIds = categories.map((item) => {
-          return mongoose.Types.ObjectId(item._id);
-        });
-        pipeline.push({
-          $match: {
-            category_id: {
-              $in: categoryIds,
-            },
-          },
-        });
-      }
-      if (query.brand?.length) {
-        let brands = query.brand.split(",");
-        if (brands.length > 0) {
-          pipeline[0]["$match"]["product_details.brand"] = {
-            $in: brands.map((brand) => new RegExp(brand, "i")),
-          };
-        }
-      }
-      if (query.store?.length) {
-        let stores = query.store.split(",");
-        if (stores.length > 0) {
-          pipeline[0]["$match"]["store"] = {
-            $in: stores.map((store) => new RegExp(store, "i")),
-          };
-        }
-      }
-      if (query.resolution?.length) {
-        let resolution = query.resolution.replaceAll("-", " ").split(",");
-        if (resolution.length > 0) {
-          pipeline[0]["$match"]["product_details.resolution"] = {
-            $in: resolution.map((resolution) => new RegExp(resolution, "i")),
-          };
-        }
-      }
-      if (query.core?.length) {
-        let core = query.core.replaceAll("-", " ").split(",");
-        if (core.length > 0) {
-          pipeline[0]["$match"]["product_details.processor core"] = {
-            $in: core.map((core) => new RegExp(core, "i")),
-          };
-        }
-      }
-      if (query.os?.length) {
-        let os = query.os.replaceAll("-", " ").split(",");
-        if (os.length > 0) {
-          pipeline[0]["$match"]["product_details.operating system"] = {
-            $in: os.map((os) => new RegExp(os, "i")),
-          };
-        }
-      }
-      if (query.battery?.length) {
-        let battery = query.battery.replaceAll("-", " ").split(",");
-        if (battery.length > 0) {
-          pipeline[0]["$match"]["product_details.battery capacity"] = {
-            $in: battery.map((battery) => new RegExp(battery, "i")),
-          };
-        }
-      }
-      if (query.processorBrand?.length) {
-        let processorBrand = query.processorBrand
-          .replaceAll("-", " ")
-          .split(",");
-        if (processorBrand.length > 0) {
-          pipeline[0]["$match"]["product_details.processor brand"] = {
-            $in: processorBrand.map(
-              (processorBrand) => new RegExp(processorBrand, "i")
-            ),
-          };
-        }
-      }
-      if (query.ram?.length) {
-        let rams = query.ram.split(",");
-        if (rams.length > 0) {
-          pipeline[0]["$match"]["product_details.ram"] = {
-            $in: rams.map((ram) => +ram),
-          };
-        }
-      }
-      if (query.rom?.length) {
-        let roms = query.rom.split(",");
-        if (roms.length > 0) {
-          pipeline[0]["$match"]["product_details.rom"] = {
-            $in: roms.map((rom) => +rom),
-          };
-        }
-      }
-      if (!query.minPrice?.toString()) {
-        query.minPrice = 5000;
-      }
-      if (query.minPrice || query.maxPrice) {
-        const priceFilter = {};
-        if (query.maxPrice) {
-          priceFilter.$lt = +query.maxPrice;
-        }
-        if (query.minPrice) {
-          priceFilter.$gt = +query.minPrice;
-        }
-
-        pipeline.push({
-          $match: {
-            price: priceFilter,
-          },
-        });
-      }
-      if (query.minRating || query.maxRating) {
-        const ratingFilter = {};
-        if (query.maxRating) {
-          ratingFilter.$lt = +query.maxRating;
-        }
-        if (query.minRating) {
-          ratingFilter.$gt = +query.minRating;
-        }
-
-        pipeline.push({
-          $match: {
-            rating: ratingFilter,
-          },
-        });
-      }
-      pipeline.push({ $skip: +skip });
-      pipeline.push({ $limit: +limit });
-      pipeline.push({ $sort: { rating: -1 } });
-    }
     try {
+      const skip = (page - 1) * limit;
+      let pipeline = [];
+      if (!query.category_id) {
+        query.category_id = "mobile";
+      } else {
+        query.category_id = query.category_id.replaceAll("_", " ");
+      }
+      let regex,
+        categories = [];
+      if (query.category_id.includes(",")) {
+        query.category_id = query.category_id.split(",");
+        for (let i = 0; i < query.category_id.length; i++) {
+          regex = new RegExp(query.category_id[i], "i");
+          const tempCategory = await CategoryModel.find({ name: regex });
+          if (tempCategory.length) {
+            categories = [...categories, ...tempCategory];
+          }
+        }
+      } else {
+        regex = new RegExp(query.category_id, "i");
+        categories =
+          query.category == "all"
+            ? await CategoryModel.find()
+            : await CategoryModel.find({ name: regex });
+      }
+      let categoryIds = [];
+      if (query) {
+        if (categories.length) {
+          categoryIds = categories.map((item) => {
+            return mongoose.Types.ObjectId(item._id);
+          });
+          pipeline.push({
+            $match: {
+              category_id: {
+                $in: categoryIds,
+              },
+            },
+          });
+        }
+        if (query.brand?.length) {
+          let brands = query.brand.split(",");
+          if (brands.length > 0) {
+            pipeline[0]["$match"]["product_details.brand"] = {
+              $in: brands.map((brand) => new RegExp(brand, "i")),
+            };
+          }
+        }
+        if (query.store?.length) {
+          let stores = query.store.split(",");
+          if (stores.length > 0) {
+            pipeline[0]["$match"]["store"] = {
+              $in: stores.map((store) => new RegExp(store, "i")),
+            };
+          }
+        }
+        if (query.resolution?.length) {
+          let resolution = query.resolution.replaceAll("-", " ").split(",");
+          if (resolution.length > 0) {
+            pipeline[0]["$match"]["product_details.resolution"] = {
+              $in: resolution.map((resolution) => new RegExp(resolution, "i")),
+            };
+          }
+        }
+        if (query.core?.length) {
+          let core = query.core.replaceAll("-", " ").split(",");
+          if (core.length > 0) {
+            pipeline[0]["$match"]["product_details.processor core"] = {
+              $in: core.map((core) => new RegExp(core, "i")),
+            };
+          }
+        }
+        if (query.os?.length) {
+          let os = query.os.replaceAll("-", " ").split(",");
+          if (os.length > 0) {
+            pipeline[0]["$match"]["product_details.operating system"] = {
+              $in: os.map((os) => new RegExp(os, "i")),
+            };
+          }
+        }
+        if (query.battery?.length) {
+          let battery = query.battery.replaceAll("-", " ").split(",");
+          if (battery.length > 0) {
+            pipeline[0]["$match"]["product_details.battery capacity"] = {
+              $in: battery.map((battery) => new RegExp(battery, "i")),
+            };
+          }
+        }
+        if (query.processorBrand?.length) {
+          let processorBrand = query.processorBrand
+            .replaceAll("-", " ")
+            .split(",");
+          if (processorBrand.length > 0) {
+            pipeline[0]["$match"]["product_details.processor brand"] = {
+              $in: processorBrand.map(
+                (processorBrand) => new RegExp(processorBrand, "i")
+              ),
+            };
+          }
+        }
+        if (query.ram?.length) {
+          let rams = query.ram.split(",");
+          if (rams.length > 0) {
+            pipeline[0]["$match"]["product_details.ram"] = {
+              $in: rams.map((ram) => +ram),
+            };
+          }
+        }
+        if (query.rom?.length) {
+          let roms = query.rom.split(",");
+          if (roms.length > 0) {
+            pipeline[0]["$match"]["product_details.rom"] = {
+              $in: roms.map((rom) => +rom),
+            };
+          }
+        }
+        if (!query.minPrice?.toString()) {
+          query.minPrice = 5000;
+        }
+        if (query.minPrice || query.maxPrice) {
+          const priceFilter = {};
+          if (query.maxPrice) {
+            priceFilter.$lt = +query.maxPrice;
+          }
+          if (query.minPrice) {
+            priceFilter.$gt = +query.minPrice;
+          }
+
+          pipeline.push({
+            $match: {
+              price: priceFilter,
+            },
+          });
+        }
+        if (query.minRating || query.maxRating) {
+          const ratingFilter = {};
+          if (query.maxRating) {
+            ratingFilter.$lt = +query.maxRating;
+          }
+          if (query.minRating) {
+            ratingFilter.$gt = +query.minRating;
+          }
+
+          pipeline.push({
+            $match: {
+              rating: ratingFilter,
+            },
+          });
+        }
+        pipeline.push({ $skip: +skip });
+        pipeline.push({ $limit: +limit });
+        pipeline.push({ $sort: { rating: -1 } });
+      }
+
       const countPipline = pipeline.filter((item) => {
-        if (item?.$skip?.toString() || item?.$limit?.toString()) {
-          return false;
-        } else {
-          return true;
+        if(item?.['$match']?.['category_id'] || item?.['$sort']?.['rating']){
+          return true
+        } else{
+          return false
         }
       });
       const data = await ProductModel.aggregate([
@@ -175,9 +189,9 @@ export const ProductService = {
                 },
               },
               {
-                  $match: {
-                      count: { $gte: 20 }
-                  }
+                $match: {
+                  count: { $gte: 20 },
+                },
               },
               {
                 $addFields: {
