7851f41ed4c296860a9c2822ad70f15dba8b6aee
Nabeel Nasir <69030093+nabeelnasir9@users.noreply.github.com>
2024-04-23 11:40:43 +0500

    updates

--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -4,25 +4,26 @@ import Blog from '../models/Blog.js';
 const router = express.Router();
 
 router.post('/blogs', async (req, res) => {
-    const { title, content, summary, author } = req.body;
-  
-    // Create a new blog post using the request's body
-    const newBlog = new Blog({
-      title,
-      content,
-      summary,
-      author: author || 'Dealsbajar',
-    });
-  
-    try {
-      const savedBlog = await newBlog.save();
-      res.status(201).json(savedBlog);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
-  });
+  const { title, content, summary, author, imageUrl } = req.body;
 
-// Get all blogs
+  const newBlog = new Blog({
+    title,
+    content,
+    summary,
+    author: author || 'Dealsbajar',
+    imageUrl
+  });
+  // console.log("Generated Slug:", newBlog.slug);
+
+  try {
+    const savedBlog = await newBlog.save();
+    res.status(201).json(savedBlog);
+  } catch (err) {
+    console.error('Error saving blog:', err);
+    res.status(400).json({ message: err.message });
+  }
+});
+
 router.get('/blogs', async (req, res) => {
   try {
     const blogs = await Blog.find();
@@ -32,15 +33,27 @@ router.get('/blogs', async (req, res) => {
   }
 });
 
-// Get a specific blog
-router.get('/blog/:id', async (req, res) => {
+// router.get('/blog/:id', async (req, res) => {
+//   try {
+//     const blog = await Blog.findById(req.params.id);
+//     if (!blog) return res.status(404).json({ message: "Blog not found" });
+//     res.json(blog);
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// });
+
+router.get('/blog/:slug', async (req, res) => {
   try {
-    const blog = await Blog.findById(req.params.id);
-    if (!blog) return res.status(404).json({ message: "Blog not found" });
+    const blog = await Blog.findOne({ slug: req.params.slug });
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
     res.json(blog);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
+
 
 export default router;
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,15 +1,45 @@
+// import { Router } from 'express';
+// const router = Router();
+// import { ProductModel } from '../models/product.model.js';
+
+// router.get('/search', async (req, res) => {
+//   try {
+//     const searchQuery = req.query.q;
+//     const products = await ProductModel.find({
+//       $text: { $search: searchQuery }
+//     }).sort({
+//       score: { $meta: "textScore" }
+//     });
+//     res.json(products);
+//   } catch (err) {
+//     res.status(500).send(err.message);
+//   }
+// });
+
+// export default router;
+
+
+
+
 import { Router } from 'express';
-const router = Router();
 import { ProductModel } from '../models/product.model.js';
+import { ObjectId } from 'mongodb';
+
+const router = Router();
 
 router.get('/search', async (req, res) => {
   try {
-    const searchQuery = req.query.q;
-    const products = await ProductModel.find({
-      $text: { $search: searchQuery }
-    }).sort({
+    const { q: searchQuery, category } = req.query;
+    let queryOptions = { $text: { $search: searchQuery } };
+
+    if (category) {
+      queryOptions.category_id = new ObjectId(category);
+    }
+
+    const products = await ProductModel.find(queryOptions).sort({
       score: { $meta: "textScore" }
     });
+
     res.json(products);
   } catch (err) {
     res.status(500).send(err.message);
