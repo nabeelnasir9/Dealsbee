5016b99d95d0345ac3f1eebb4e225308f4842261
Nabeel Nasir <69030093+nabeelnasir9@users.noreply.github.com>
2024-05-01 23:09:53 +0500

    newscrap: (New mobile products)

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ import scraperRoute from "./scraper/index.js";
 import categoryRoute from "./category/index.js";
 import productRoute from "./product/index.js";
 import comparisonRoute from "./comparison/index.js";
-import smartphoneRouter from "./smartphone/index.js";
+// import smartphoneRouter from "./smartphone/index.js";
 const protectedRouter = express.Router();
 const unProtectedRouter = express.Router();
 
@@ -19,6 +19,6 @@ unProtectedRouter.use("/scraper", scraperRoute);
 unProtectedRouter.use("/category", categoryRoute);
 unProtectedRouter.use("/product", productRoute);
 unProtectedRouter.use("/comparison", comparisonRoute);
-unProtectedRouter.use("/smartphones", smartphoneRouter);
+// unProtectedRouter.use("/smartphones", smartphoneRouter);
 
 export { protectedRouter, unProtectedRouter };
---
+++ src/routes/smartphone/smartphoneController.js
@@ -0,0 +1,60 @@
+import * as smartphoneService from '../../services/smartphone.service.js';
+
+export const getSmartphones = async (req, res) => {
+    try {
+      const { limit, page } = req.query;
+      const smartphones = await smartphoneService.getAllSmartphones({ limit, page });
+      res.json(smartphones);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  };
+  
+
+export const getSmartphoneById = async (req, res) => {
+  try {
+    const smartphone = await smartphoneService.getSmartphoneById(req.params.id);
+    if (smartphone) {
+      res.json(smartphone);
+    } else {
+      res.status(404).send('Smartphone not found');
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+export const createSmartphone = async (req, res) => {
+  try {
+    const smartphone = await smartphoneService.createSmartphone(req.body);
+    res.status(201).json(smartphone);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+export const updateSmartphone = async (req, res) => {
+  try {
+    const updatedSmartphone = await smartphoneService.updateSmartphone(req.params.id, req.body);
+    if (updatedSmartphone) {
+      res.json(updatedSmartphone);
+    } else {
+      res.status(404).send('Smartphone not found');
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+export const deleteSmartphone = async (req, res) => {
+  try {
+    const result = await smartphoneService.deleteSmartphone(req.params.id);
+    if (result) {
+      res.status(204).send();
+    } else {
+      res.status(404).send('Smartphone not found');
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
