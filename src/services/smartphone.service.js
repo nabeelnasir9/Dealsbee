4c0a731e2138eeb56ab7c2a3e36ac1d59d2c18be
Nabeel Nasir <69030093+nabeelnasir9@users.noreply.github.com>
2024-05-10 10:07:50 +0500

    updates

--- src/services/smartphone.service.js
+++ src/services/smartphone.service.js
@@ -21,7 +21,13 @@ export const getAllSmartphones = (filters) => {
     resolution,
     screenSize,
     stores,
-    camera
+    camera,
+    internalMemory,
+    aspectRatio,
+    discounts,
+    display_type,
+    frontCamera,
+    availability
   } = filters;
 
   const query = {};
@@ -87,16 +93,41 @@ export const getAllSmartphones = (filters) => {
     query.Resolution = resolution;
   }
 
-  if(screenSize){
+  if (screenSize) {
     query["Screen Size"] = screenSize;
   }
-  if(stores){
-    query.stores=stores;
+  if (stores) {
+    query.stores = stores;
   }
 
-  if(camera){
-    query["Rear Camera"]= camera;
+  if (camera) {
+    query["Rear Camera"] = camera;
   }
+
+  if (internalMemory) {
+    query["Internal Memory"] = internalMemory;
+  }
+
+  if (aspectRatio) {
+    query["Aspect ratio"] = aspectRatio;
+  }
+
+  if (discounts) {
+    query.discounts = discounts;
+  }
+
+  if (display_type) {
+    query.display_type = display_type;
+  }
+
+  if (frontCamera) {
+    query.frontCamera = frontCamera;
+  }
+
+  if (availability) {
+    query.availability = availability;
+  }
+
   const options = {
     skip: (page - 1) * limit,
     limit: parseInt(limit),
@@ -105,9 +136,9 @@ export const getAllSmartphones = (filters) => {
   return Smartphone.find(query, null, options);
 };
 
-export const getSmartphoneById = (id) => {
-  return Smartphone.findById(id);
-};
+// export const getSmartphoneById = (id) => {
+//   return Smartphone.findById(id);
+// };
 
 export const createSmartphone = (data) => {
   const smartphone = new Smartphone(data);
@@ -120,4 +151,33 @@ export const updateSmartphone = (id, data) => {
 
 export const deleteSmartphone = (id) => {
   return Smartphone.findByIdAndDelete(id);
+};
+
+
+export const getSmartphoneById = async (id) => {
+  try {
+    const smartphone = await Smartphone.findById(id);
+    if (!smartphone) return null;  // Returns null if the smartphone is not found
+
+    // Extract the price of the first variant
+    const price = parseInt(smartphone.variants[0].price.replace(/₹/, ''));
+
+    // Calculate 10% range for similar price search
+    const lowerBound = price * 0.9;
+    const upperBound = price * 1.1;
+
+    // Find similar priced smartphones
+    const similarSmartphones = await Smartphone.find({
+      "variants.0.price": {
+        $gte: `₹${Math.floor(lowerBound)}`,
+        $lte: `₹${Math.ceil(upperBound)}`
+      },
+      _id: { $ne: id }  // Exclude the current smartphone
+    }).limit(8);  // Limit to 6 similar smartphones
+
+    return { smartphone, similarSmartphones };
+  } catch (error) {
+    console.error("Failed to fetch smartphone by ID with error:", error);
+    throw error;
+  }
 };
